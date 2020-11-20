7e29f3bec8ff9d73ed5e7e902c0a1e92012156a8
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-11-20 15:57:26 +1300

    Change createResponse to send all fields in `options`

--- src/pages/api/responses/default.ts
+++ src/pages/api/responses/default.ts
@@ -1,15 +1,7 @@
-type ApiResponse = (options: ApiError | ApiSuccess) => Promise<Response>;
+type ApiResponse = (options: ApiError | ApiSuccess) => Response;
 
 export const createResponse: ApiResponse = async (options) => {
-    if (options.status === "success") {
-        const { status, result } = options;
-        const json = { status, result };
-        return new Response(JSON.stringify(json));
-    }
-
-    const { status, message, httpCode } = options;
-    const json = { status, message };
-    return new Response(JSON.stringify(json), {
-        status: httpCode,
+    return new Response(JSON.stringify(options), {
+        status: options.status === "success" ? 200 : options.httpCode,
     });
 };
