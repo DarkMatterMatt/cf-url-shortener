8e44f2917c7b90d551a166eaf9a80f02eb765e6c
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-11-20 15:54:54 +1300

    Add asyncMap function

---
+++ src/helpers/array.ts
@@ -0,0 +1,13 @@
+/**
+ * Same as array.map(), but allows an async callback function
+ * @param arr Array to map over
+ * @param fn  Callback function to transform item
+ */
+export function asyncMap<T, U>(
+    arr: T[],
+    fn: (value: T, index: number, array: T[]) => Promise<U>
+): U[] {
+    return Promise.all(
+        arr.map(async (item, index) => await fn(item, index, arr))
+    );
+}
