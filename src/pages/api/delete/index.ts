3a29816d8cdd95399ec320b30569e9ac8802d075
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-11-23 18:53:58 +1300

    Implement /api/delete

--- src/pages/api/delete/index.ts
+++ src/pages/api/delete/index.ts
@@ -1,3 +1,31 @@
+import { deleteRedirect, parseShortName } from "~/db";
+import { createResponse } from "../responses";
+
 export const handleRequest: NestedHandler = async (req, path) => {
-    return new Response(`delete, path: ${path}`);
+    let shortName: string;
+
+    try {
+        const json = await req.json();
+        const shortNameOrErr = parseShortName(json.shortName);
+        if (shortNameOrErr instanceof Error) {
+            throw shortNameOrErr;
+        }
+        shortName = shortNameOrErr;
+    }
+    catch (e: unknown) {
+        return createResponse({
+            status: "error",
+            httpCode: 400,
+            message: e instanceof Error ? e.message : "",
+        });
+    }
+
+    await deleteRedirect(shortName);
+
+    return createResponse({
+        status: "success",
+        result: {
+            shortName,
+        },
+    });
 };
